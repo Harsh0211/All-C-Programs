8763ba99aae5d6bf0fdb63f07271601b314bc020
Aman <55349238+X-genius@users.noreply.github.com>
2019-10-21 22:13:56 +0530

    Update primeno.cpp

--- primeno.cpp
+++ primeno.cpp
@@ -2,20 +2,22 @@
 using namespace std;  
 int main()  
 {  
-  int n, i, m=0, flag=0;  
-  cout << "Enter the Number to check Prime: ";  
+  int n, i;  
+  cout << "Enter the Number: ";  
   cin >> n;  
-  m=n/2;  
-  for(i = 2; i <= m; i++)  
+  
+  for(i = 2; i <= n; i++)  
   {  
       if(n % i == 0)  
       {  
           cout<<"Number is not Prime."<<endl;  
-          flag=1;  
           break;  
+      } 
+      else
+      {
+           cout<<"Number is Not Prime."<<endl;
+           break;
       }  
-  }  
-  if (flag==0)  
-      cout << "Number is Prime."<<endl;  
+  }
   return 0;  
 }  
